eec7ac19035811c25bf539a54734e686e8562f5a
cat <739068681@qq.com>
2022-10-12 10:06:18 +0800

    🎉 init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<style scoped>
+@import url("@/assets/main.css");
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1 class="green">首页</h1>
+    <RouterLink to="/about">关于</RouterLink>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
